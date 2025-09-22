05a3b33ba66d2a6838461fa256e1e9c8d5afcb18
junxigary <junxixi.huang@mail.utoronto.ca>
2025-09-22 12:53:53 -0400

    Implement retrieve method: get root by path string

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,9 +88,18 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        if (path.isEmpty()) {
+            return this.root;
+        }
+        char direction = path.charAt(0);
+        String remainPath = path.substring(1);
+        if (direction == 'l') {
+            return this.left.retrieve(remainPath);
+        } else {
+            return this.right.retrieve(remainPath);
+        }
     }
+
 
     /** Read in the NumberTriangle structure from a file.
      *
